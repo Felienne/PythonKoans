3bdb5f2461dc94d99e9f48ab5098fcdd06dd5c76
Felienne <felienne@gmail.com>
2017-08-16 14:27:57 +0200

    added tests in week 2 lesson 5 and tried gitignore again

--- koans/week_2_lesson_5_about_iteration.py
+++ koans/week_2_lesson_5_about_iteration.py
@@ -15,9 +15,32 @@ class AboutIteration(Koan):
 
         self.assertEqual(__, ten_little_squares)
 
-    #TODO: hier nog een keer continue en break
-
     # 2
+    def test_for_statement_with_break(self):
+        one_to_10 = range(10)
+        four_little_squares = []
+
+        for i in one_to_10:
+            if i == 4:
+                break
+            four_little_squares.append(i*i)
+
+        self.assertEqual(__, four_little_squares)
+
+    # 3
+    def test_for_statement_with_continue(self):
+        one_to_10 = range(10)
+        I_hate_nine = []
+
+        for i in one_to_10:
+            if i == 3:
+                continue
+            I_hate_nine.append(i*i)
+
+        self.assertEqual(__, I_hate_nine)
+
+
+    # 4
     def test_for_statement_over_list(self):
         one_direction = ["harry", "niall", "louis", "liam", "zayn"]
         one_direction_print = []
@@ -27,7 +50,7 @@ class AboutIteration(Koan):
         self.assertEqual([__, __, __, __, __], one_direction_print)
 
 
-    # 3
+    # 5
     def test_iterating_with_next(self):
         stages = iter(['alpha', 'beta', 'gamma'])
 
