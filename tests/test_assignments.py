c8a53cc3bd22b86d423e48450237198baadf1047
Llewellyn Falco <isidore@setgame.com>
2017-08-18 04:21:19 +0300

    working on test

--- tests/test_assignments.py
+++ tests/test_assignments.py
@@ -2,16 +2,31 @@ from koans.week_1_lesson_2_about_assignments import AboutAssignments
 from runner.koan import *
 
 
-class TestAboutAssignments(Koan):
+class AboutAssignmentsTest(Koan):
     # 1
 
     def test_koans(self):
+        koan = AboutAssignments()
+        self.assert_koan(137, koan.test_integer_assignment)
+
+    def assert_koan(self, answer, koan):
+        self.assert_currently_failing(koan)
+        self.assert_correct_answer(answer, koan)
+
+    def assert_correct_answer(self, answer, koan):
+        global __
+        reset = __
+        __ = answer
+        print("******* Answer is " + str(__))
+        try:
+            koan()
+        finally:
+            __ = reset
+
+    def assert_currently_failing(self, koan):
         failed = False
         try:
-            AboutAssignments().test_integer_assignment()
+            koan()
         except:
             failed = True
-
-        self.assertEqual(failed,True)
-        x = __
-        AboutAssignments().test_integer_assignment()
+        self.assertTrue(failed, "The koan '{}' was left pre-answered".format(koan.__name__))
