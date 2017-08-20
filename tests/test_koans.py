a51a37bcb3f6fef712c729dcbc120f638fdebd7c
Llewellyn Falco <isidore@setgame.com>
2017-08-20 20:57:19 +0300

    better solution for testing

--- tests/test_koans.py
+++ tests/test_koans.py
@@ -1,11 +1,19 @@
 from koans.week_1_lesson_2_about_assignments import AboutAssignments
+from koans import week_1_lesson_2_about_assignments
+from runner import koan
 from runner.koan import *
 
 
+
 class KoanTest(Koan):
+    def set(self, values):
+        self.lesson.__ = values[0]
+        self.lesson.______ = values[1]
+        self.lesson._______ = values[2]
+
     def test_lesson_2_assignments(self):
+        self.lesson = week_1_lesson_2_about_assignments
         koan = AboutAssignments()
-        self.setter = koan.set
         self.assert_koan(koan.test_integer_assignment, 137)
         self.assert_koan(koan.test_integer_instance_of, 5)
         self.assert_koan(koan.test_float_assignment, 3.14)
@@ -25,11 +33,11 @@ class KoanTest(Koan):
 
     def assert_correct_answer(self, answer, koan):
         reset = [__, ______, _______]
-        self.setter(answer)
+        self.set(answer)
         try:
             koan()
         finally:
-            self.setter(reset)
+            self.set(reset)
 
 
     def assert_currently_failing(self, koan):
