94e47247574735d04f47246102b2da7144fc012f
Felienne <felienne@gmail.com>
2017-08-21 09:09:47 +0200

    week 5 ready to enter in Stepik, so it week 4 except lambdas

--- koans/week_5_lesson_1_about_classes.py
+++ koans/week_5_lesson_1_about_classes.py
@@ -5,90 +5,75 @@ class Dog:
     "Dogs need regular walks. Never, ever let them drive."
 
 class Cat:
-    def __init__(self, name):
-        self.name = name
+    def __init__(self, n):
+        self.name = n
+
+    def what_am_I(self):
+        return self
 
 class FancyCat:
-    def __init__(self, name, title):
-        self.name = name
-        self.title = name
+    def __init__(self, n, t):
+        self.name = n
+        self.title = t
 
     def get_full_name(self):
         return "{0}. {1}".format(self.title,self.name)
 
-class PrintableDog:
-    def __init__(self, initial_name):
-        self._name = initial_name
-
-    def __str__(self):
-        #
-        # Implement this!
-        #
-        return __
-
-    def __repr__(self):
-        return "A dog named '" + self._name + "'"
-
 
 class AboutClasses(Koan):
     #1
-    def test_instances_of_classes_can_be_created_adding_parentheses(self):
+    def test_instances_of_classes_can_be_created_with_parentheses(self):
         fido = Dog()
-        self.assertEqual(__, type(fido).__name__)
-        self.assertEqual(__, type(fido))
+        self.assertEqual(Dog, __)
 
     #2
     def test_classes_have_documentation_too(self):
         self.assertRegex(Dog.__doc__, __)
 
-    #3 ------------------------------------------------------------------
-    def test_init_method_is_the_constructor(self):
+    #3
+    def test_classes_are_types(self):
+        self.assertEqual(__, self.Dog.__class__ == type)
+
+    #4
+    def test_init_method_creates_class(self):
         felix = Cat("Felix Domestica Prima")
         self.assertEqual(__, felix.name)
 
-    #4
+    #5
     def test_you_can_also_access_the_value_out_using_getattr_and_dict(self):
         felix = Cat("Felix Domestica Prima")
         self.assertEqual(__, felix.name)
         self.assertEqual(__, getattr(felix, "name"))
 
 
-    #5
+    #6
     def test_classes_can_have_methods(self):
         evil_cat = FancyCat("Mr.","Bigglesworth")
         self.assertEqual(__, evil_cat.get_full_name())
 
-    #6
+    #7
     def test_different_objects_have_different_instance_variables(self):
         evil_cat = FancyCat("Mr.","Bigglesworth")
         another_evil_cat = FancyCat("Mrs", "Norris")
 
         self.assertEqual(__, evil_cat.name == another_evil_cat.name)
 
-    # ------------------------------------------------------------------
-    #7
-    def test_str_provides_a_string_version_of_the_object(self):
-        sue = PrintableDog("Sue")
-
-        self.assertEqual("Sue", str(sue))
-
-    #8
-    def test_str_is_used_explicitly_in_string_interpolation(self):
-        sue = PrintableDog("Sue")
-
-        self.assertEqual(__, "My name is " + str(sue) + ". How do you do?")
-
     #9
-    def test_repr_provides_a_more_complete_string_version(self):
-        sue = PrintableDog("Sue")
-        self.assertEqual(__, repr(sue))
+    def test_self_returns_object(self):
+        freckles = Cat('Freckles')
+        self.assertEqual(__, type(freckles.what_am_I()))
 
-    #10
-    def test_all_objects_support_str_and_repr(self):
-        seq = [1, 2, 3]
+    #10--------------
 
-        self.assertEqual(__, str(seq))
-        self.assertEqual(__, repr(seq))
+    class Turtle:
+        def __init__(self, n):
+            self.name = n
 
-        self.assertEqual(__, str("STRING"))
-        self.assertEqual(__, repr("STRING"))
+    #11
+    def test_classes_may_be_nested(self):
+        sheldon = self.Turtle('Sheldon')
+        self.assertEqual(__, type(sheldon))
+
+    #12
+    def test_what_is_self_here(self):
+        self.assertEqual(__, type(self))
---
+++ koans/week_5_lesson_3_about_inheritance.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from runner.koan import *
+
+class AboutInheritance(Koan):
+    class Dog:
+        def __init__(self, name):
+            self._name = name
+
+        @property
+        def name(self):
+            return self._name
+
+        def bark(self):
+            return "WOOF"
+
+    class Chihuahua(Dog):
+        def wag(self):
+            return "happy"
+
+        def bark(self):
+            return "woof woof"
+
+
+    def test_dogs_are_still_dogs(self):
+        boef = self.Dog('Boef')
+        self.assertEqual(self.Dog, __)
+
+    def test_but_what_are_chihuahuas(self):
+        tinkerbell = self.Chihuahua('Tinkerbell')
+        self.assertEqual(__, type(tinkerbell))
+
+    def test_subclasses_have_the_parent_as_an_ancestor(self):
+        self.assertEqual(__, issubclass(self.Chihuahua, self.Dog))
+
+    def test_all_defined_classes_ultimately_inherit_from_object_class(self):
+        self.assertEqual(__, issubclass(self.Chihuahua, object))
+
+    def test_all_built_in_classes_ultimately_inherit_from_object_class(self):
+        self.assertEqual(__, issubclass(str, object))
+
+
+    def test_instances_inherit_behavior_from_parent_class(self):
+        chico = self.Chihuahua("Chico")
+        self.assertEqual(__, chico.name)
+
+    def test_subclasses_add_new_behavior(self):
+        chico = self.Chihuahua("Chico")
+        self.assertEqual(__, chico.wag())
+
+        fido = self.Dog("Fido")
+        with self.assertRaises(___): fido.wag()
+
+    def test_subclasses_can_modify_existing_behavior(self):
+        chico = self.Chihuahua("Chico")
+        self.assertEqual(__, chico.bark())
+
+        fido = self.Dog("Fido")
+        self.assertEqual(__, fido.bark())
+
+    # ------------------------------------------------------------------
+
+    class BullDog(Dog):
+        def bark(self):
+            return super().bark() + ", GRR"
+
+    def test_subclasses_can_invoke_parent_behavior_via_super(self):
+        ralph = self.BullDog("Ralph")
+        self.assertEqual(__, ralph.bark())
+
+    # ------------------------------------------------------------------
+
+    class GreatDane(Dog):
+        def growl(self):
+            return super().bark() + ", GROWL"
+
+    def test_super_works_across_methods(self):
+        george = self.GreatDane("George")
+        self.assertEqual(__, george.growl())
+
+    # ---------------------------------------------------------
+
+    class Pug(Dog):
+        def __init__(self, name):
+            pass
+
+    class Greyhound(Dog):
+        def __init__(self, name):
+            super().__init__(name)
+
+    def test_base_init_does_not_get_called_automatically(self):
+        snoopy = self.Pug("Snoopy")
+        with self.assertRaises(___): name = snoopy.name
+
+    def test_base_init_has_to_be_called_explicitly(self):
+        boxer = self.Greyhound("Boxer")
+        self.assertEqual(__, boxer.name)
